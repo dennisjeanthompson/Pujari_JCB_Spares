d1c22fc7a1fafd8b9dd1c72c06d0ff1dee23443a
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-07-06 16:00:23 +0530

    Add files via upload

---
+++ Backend/Order Backend/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const { MongoClient, ObjectId } = require('mongodb')
+const mongodb = require('mongodb')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+const dbUrl = 'mongodb+srv://rohit10231:rohitkaranpujari@cluster0.kjynvxt.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(dbUrl)
+const port = 7500
+
+// getting all users order information
+app.get('/', async (req, res) => {
+    const client = await MongoClient.connect(dbUrl)
+    try {
+        const db = await client.db('Pujari_JCB_Spares')
+        let orders = await db.collection('Orders').find().toArray()
+        if (orders.length !== 0) {
+            res.status(200).send(orders)
+        }
+        else {
+            res.send({ message: "No orders placed yet !" })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'Internal server error', error })
+    }
+    finally {
+        client.close()
+    }
+})
+
+// booking new order
+app.post('/newOrder', async (req, res) => {
+    const client = await MongoClient.connect(dbUrl)
+    try {
+        const db = await client.db('Pujari_JCB_Spares')
+        await db.collection('Orders').insertOne(req.body)
+        res.status(201).send({ message: 'Order placed', data: req.body })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'Internal server error', error })
+    }
+    finally {
+        client.close()
+    }
+})
+
+// getting orders data of user
+app.get('/getOrders/:email', async (req, res) => {
+    const client = await MongoClient.connect(dbUrl)
+    try {
+        const db = await client.db('Pujari_JCB_Spares')
+        let order = await db.collection('Orders').aggregate([{ $match: { email: req.params.email } }]).toArray()
+        if (order.length!==0) {
+            res.status(200).send(order)
+        }
+        else {
+            res.send({ message: 'No order placed' })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'Internal server error', error })
+    }
+    finally {
+        client.close()
+    }
+})
+
+app.listen(port, () => { console.log(`App listening on ${port}`) })
