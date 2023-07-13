b093bb7d33c39f65935e2d849c55ac564dc30e7e
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-07-13 10:03:08 +0530

    Update index.js

--- Backend/Order Backend/index.js
+++ Backend/Order Backend/index.js
@@ -14,7 +14,8 @@ app.get('/', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Pujari_JCB_Spares')
-        let orders = await db.collection('Orders').find().toArray()
+        let orders = await db.collection('Orders').aggregate([{ $sort: { date: -1 } }]).toArray()
+        // find().toArray()
         if (orders.length !== 0) {
             res.status(200).send(orders)
         }
@@ -36,6 +37,8 @@ app.post('/newOrder', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Pujari_JCB_Spares')
+        req.body.price = parseInt(req.body.price)
+        req.body.quantity = parseInt(req.body.quantity)
         await db.collection('Orders').insertOne(req.body)
         res.status(201).send({ message: 'Order placed', data: req.body })
     }
@@ -76,8 +79,8 @@ app.delete('/cancleOrder/:orderId', async (req, res) => {
     try {
         const db = await client.db('Pujari_JCB_Spares')
         let order = await db.collection('Orders').deleteOne({ _id: mongodb.ObjectId(req.params.orderId) })
-        res.status(200).send({message:'order cancelled'})
-        
+        res.status(200).send({ message: 'order cancelled' })
+
     }
     catch (error) {
         console.log(error);
@@ -87,5 +90,46 @@ app.delete('/cancleOrder/:orderId', async (req, res) => {
         client.close()
     }
 })
+
+// setTimeout(async (req, res) => {
+//     let serviceRunDate = new Date().getDate()
+//     const client = await MongoClient.connect(dbUrl)
+//     try {
+//         const db = await client.db('Pujari_JCB_Spares')
+//         let orders = await db.collection('Orders').find().toArray()
+//         if (orders.length !== 0) {
+//             orders.forEach((order) => {
+//                 let orderDate = new Date(order.date).getDate()
+//                 let dateDiff = serviceRunDate - orderDate;
+//                 if (dateDiff >= 5) {
+//                     // change delivered status to true
+//                     // console.log(order._id);
+//                     // db.collection('Orders').updateOne({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+//                     // aggregate([{ $match: { email: req.params.email } }]).toArray()
+
+//                     // console.log('order delivered');
+//                     // console.log(order);
+//                 }
+//                 else {
+//                     // change delivered status to false
+
+//                     // console.log('not delivered');
+//                 }
+//             })
+//         }
+//         else {
+//             // res.send({ message: "No orders placed yet !" })
+//             console.log('no orders');
+
+//         }
+//     }
+//     catch (error) {
+//         console.log(error);
+//         // res.send({ message: 'Internal server error', error })
+//     }
+//     finally {
+//         client.close()
+//     }
+// }, 2000)
 
 app.listen(port, () => { console.log(`App listening on ${port}`) })
