909f80f80454ae72f8318c1aa979f20ee60a7b0d
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-10-15 21:33:29 +0530

    Update Login.js

--- Frontend/src/components/Login/Login.js
+++ Frontend/src/components/Login/Login.js
@@ -14,6 +14,7 @@ function Login() {
 
     useEffect(() => {
         getAllProducts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     async function getAllProducts() {
